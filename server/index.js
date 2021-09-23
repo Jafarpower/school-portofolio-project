394fdd5bb99f22fa389881d628d8e049fa509371
Jafar <jafaralkhafajidev@hotmail.com>
2021-09-23 17:34:42 +0200

    Added project to repository

---
+++ server/index.js
@@ -0,0 +1,22 @@
+// server/index.js
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/index.html"));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/about.html"));
+});
+
+app.use(express.static(__dirname + "/../client"));
+
+//add the router
+app.use("/", router);
+app.listen(process.env.port || 3000);
+
+console.log("Running at Port 3000");
